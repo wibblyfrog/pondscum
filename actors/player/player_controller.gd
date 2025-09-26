c98cfad4512fb9c55cccec1277e37bf1b12a27a8
wibblyfrog <kaime.r.welsh@gmail.com>
2025-09-25 18:44:57 -0700

    Refactor player controller and scene structure; begin implementing multiplayer server/client functionality

--- actors/player/player_controller.gd
+++ actors/player/player_controller.gd
@@ -5,10 +5,10 @@ class_name PlayerController
 	set(id):
 		player = id
 
-const MOVE_SPEED := 12
-const JUMP_FORCE := 30
-const GRAVITY := 0.98
-const MAX_FALL_SPEED := 30
+@export var MOVE_SPEED := 12
+@export var JUMP_FORCE := 15
+@export var GRAVITY := 0.98
+@export var MAX_FALL_SPEED := 30
 
 var mouse_sensitivity: float = 0.05
 var min_yaw: float = 0
@@ -25,6 +25,7 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event: InputEvent) -> void:
+	if is_multiplayer_authority() == false: return
 	if Input.is_action_just_pressed("ui_cancel"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -34,27 +35,17 @@ func _unhandled_input(event: InputEvent) -> void:
 	# Mouse Look
 	if event is InputEventMouseMotion:
 		var pcam_rotation_degrees: Vector3
-
-		# Assigns the current 3D rotation of the SpringArm3D node - to start off where it is in the editor.
 		pcam_rotation_degrees = pcam.get_third_person_rotation_degrees()
-
-		# Change the X rotation.
 		pcam_rotation_degrees.x -= event.relative.y * mouse_sensitivity
-			
-		# Clamp the rotation in the X axis so it can go over or under the target.
 		pcam_rotation_degrees.x = clampf(pcam_rotation_degrees.x, min_pitch, max_pitch)
-
-		# Change the Y rotation value.
 		pcam_rotation_degrees.y -= event.relative.x * mouse_sensitivity
-			
-		# Sets the rotation to fully loop around its target, but without going below or exceeding 0 and 360 degrees respectively.
 		pcam_rotation_degrees.y = wrapf(pcam_rotation_degrees.y, min_yaw, max_yaw)
-			
-		# Change the SpringArm3D node's rotation and rotate around its target.
 		pcam.set_third_person_rotation_degrees(pcam_rotation_degrees)
 	
 
 func _physics_process(_delta: float) -> void:
+	if is_multiplayer_authority() == false: return
+
 	# Movement
 	var move_vector := Vector3.ZERO
 	if Input.is_action_pressed("move_forward"):
